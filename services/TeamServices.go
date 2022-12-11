c676b8de219e165cf0a5e6c84baca7f86c83d94e
Mike Buell <michael.buell@gmail.com>
2022-12-11 09:31:46 -0500

    Added os.exit for errors for now

--- services/TeamServices.go
+++ services/TeamServices.go
@@ -16,6 +16,7 @@ func makeRequest(url string) []byte {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 	req.Header.Add("accept", "application/json")
 	req.Header.Add("X-TBA-Auth-Key", "hJm8rF1L3SiiqMDGEXc5LIwWvhCQBHyNDdJ9WaaiV8DuvnL2URPyMYkqGQoxlYhh")
@@ -31,6 +32,7 @@ func makeRequest(url string) []byte {
 	fmt.Printf("bodybytes: %+v\n", string(bodyBytes))
 	if err != nil {
 		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 	return bodyBytes
 }
