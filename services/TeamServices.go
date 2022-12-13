516015d5a17b70fd344107872d0659ee738a84bd
Mike Buell <michael.buell@gmail.com>
2022-12-12 21:31:31 -0500

    Refactored API call

--- services/TeamServices.go
+++ services/TeamServices.go
@@ -2,9 +2,6 @@ package services
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 )
 
 const GETTEAMURL string = "https://www.thebluealliance.com/api/v3/team/%s"
@@ -14,64 +11,38 @@ const GETTEAMEVENTSSIMPLEURL string = "https://www.thebluealliance.com/api/v3/te
 const GETTEAMEVENTSYEARURL string = "https://www.thebluealliance.com/api/v3/team/%s/events/%d"
 const GETTEAMEVENTSYEARSIMPLEURL string = "https://www.thebluealliance.com/api/v3/team/%s/events/%d/simple"
 
-func makeRequest(url string) []byte {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	req.Header.Add("accept", "application/json")
-	req.Header.Add("X-TBA-Auth-Key", "hJm8rF1L3SiiqMDGEXc5LIwWvhCQBHyNDdJ9WaaiV8DuvnL2URPyMYkqGQoxlYhh")
-	// fmt.Printf("req: %+v\n", req)
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	// fmt.Printf("resp: %+v\n", resp)
-	defer resp.Body.Close()
-	bodyBytes, err := io.ReadAll(resp.Body)
-	fmt.Printf("bodybytes: %+v\n", string(bodyBytes))
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	return bodyBytes
-}
-
 func GetTeamInfo(teamKey string) []byte {
 	var url string = fmt.Sprintf(GETTEAMURL, teamKey)
-	var bodyBytes []byte = makeRequest(url)
+	var bodyBytes []byte = TBAMakeRequest(url)
 	return bodyBytes
 }
 
 func GetTeamYearsParticipated(teamKey string) []byte {
 	var url string = fmt.Sprintf(GETTEAMYEARSURL, teamKey)
-	var bodyBytes []byte = makeRequest(url)
+	var bodyBytes []byte = TBAMakeRequest(url)
 	return bodyBytes
 }
 
 func GetTeamEvents(teamKey string) []byte {
 	var url string = fmt.Sprintf(GETTEAMEVENTSURL, teamKey)
-	var bodyBytes []byte = makeRequest(url)
+	var bodyBytes []byte = TBAMakeRequest(url)
 	return bodyBytes
 }
 
 func GetTeamEventsSimple(teamKey string) []byte {
 	var url string = fmt.Sprintf(GETTEAMEVENTSSIMPLEURL, teamKey)
-	var bodyBytes []byte = makeRequest(url)
+	var bodyBytes []byte = TBAMakeRequest(url)
 	return bodyBytes
 }
 
 func GetTeamEventsForYear(teamKey string, year int) []byte {
 	var url string = fmt.Sprintf(GETTEAMEVENTSYEARURL, teamKey, year)
-	var bodyBytes []byte = makeRequest(url)
+	var bodyBytes []byte = TBAMakeRequest(url)
 	return bodyBytes
 }
 
 func GetTeamEventsForYearSimple(teamKey string, year int) []byte {
 	var url string = fmt.Sprintf(GETTEAMEVENTSYEARSIMPLEURL, teamKey, year)
-	var bodyBytes []byte = makeRequest(url)
+	var bodyBytes []byte = TBAMakeRequest(url)
 	return bodyBytes
 }
