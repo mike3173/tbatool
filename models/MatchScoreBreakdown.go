67136ccbf13f329d2836a387d4a4bce77415c7f6
Mike Buell <michael.buell@gmail.com>
2022-12-20 11:21:25 -0500

    Removed unnecessary fields

--- models/MatchScoreBreakdown.go
+++ models/MatchScoreBreakdown.go
@@ -3,6 +3,4 @@ package models
 type MatchScoreBreakdown struct {
 	Blue map[string]interface{} `json:"blue"`
 	Red  map[string]interface{} `json:"red"`
-	// Blue MatchScoreBreakdown2022 `json:"blue"`
-	// Red  MatchScoreBreakdown2022 `json:"red"`
 }
