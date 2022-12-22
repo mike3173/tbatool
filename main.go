11a5a53b7057dd02bcddb20dde3a4f5c99f7f4f2
Mike Buell <michael.buell@gmail.com>
2022-12-22 14:22:22 -0500

    Match testing

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"sort"
 
 	"github.com/mike3173/tbatool/models"
+	"github.com/mike3173/tbatool/report"
 	"github.com/mike3173/tbatool/services"
 )
 
 func main() {
 	fmt.Println("Calling API...")
-	var bodyBytes []byte
 
 	// bodyBytes = services.GetTeamInfo("frc3173")
 	// var t models.Team
@@ -28,52 +27,79 @@ func main() {
 	// json.Unmarshal(bodyBytes, &events)
 	// fmt.Printf("events: %+v\n\n", events)
 
-	bodyBytes = services.GetMatch("2022nyro", "qm", 43)
-	var match models.Match
-	err := json.Unmarshal(bodyBytes, &match)
+	var bodyBytes []byte = services.GetEventMatches("2022nyro")
+	var matches []models.Match
+	err := json.Unmarshal(bodyBytes, &matches)
 	if err != nil {
 		fmt.Println(err)
 		return
 	} else {
-		fmt.Printf("match.key: %s\n", match.Key)
-		fmt.Printf("match.Alliances: BLUE %+v\n", match.Alliances.Blue)
-		fmt.Printf("match.Alliances: RED  %+v\n", match.Alliances.Red)
-		fmt.Printf("match.scorebreakdown: BLUE %+v\n", match.ScoreBreakdown.Blue)
-		fmt.Printf("match.scorebreakdown: RED  %+v\n", match.ScoreBreakdown.Red)
-		sortedKeys := make([]string, 0, len(match.ScoreBreakdown.Blue))
-		fmt.Printf("\nmatch score key list:\n")
-		for key := range match.ScoreBreakdown.Blue {
-			sortedKeys = append(sortedKeys, key)
-			fmt.Printf("%s ", key)
+		for i := 0; i < len(matches); i++ {
+			report.MatchReportCsv(matches[i])
 		}
-		sort.Strings(sortedKeys)
-		fmt.Println()
-		fmt.Printf("match score value list:\n")
-		for _, key := range sortedKeys {
-			fmt.Printf(" %+v", match.ScoreBreakdown.Blue[key])
-		}
-		fmt.Println()
-
-		// var blueMap map[string]interface{}
-		// var redMap map[string]interface{}
-		// dataBlue, _ := json.Marshal(match.ScoreBreakdown.Blue)
-		// json.Unmarshal(dataBlue, &blueMap)
-		// dataRed, _ := json.Marshal(match.ScoreBreakdown.Red)
-		// json.Unmarshal(dataRed, &redMap)
-		// fmt.Printf("match.scorebreakdown: BLUE %+v\n", blueMap)
-		// fmt.Printf("match.scorebreakdown: RED  %+v\n", redMap)
-		// sortedKeys := make([]string, 0, len(blueMap))
-		// for key := range blueMap {
-		// 	sortedKeys = append(sortedKeys, key)
-		// 	fmt.Printf("%s ", key)
-		// }
-		// sort.Strings(sortedKeys)
-		// fmt.Println()
-		// for _, key := range sortedKeys {
-		// 	fmt.Printf(" %+v", blueMap[key])
-		// }
-		// fmt.Println()
 	}
+
+	// bodyBytes []byte = services.GetMatch("2022nyro", "qm", 43)
+	// var match models.Match
+	// err := json.Unmarshal(bodyBytes, &match)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// } else {
+	// 	fmt.Printf("match.key: %s\n", match.Key)
+	// 	fmt.Printf("match.Alliances: BLUE %+v\n", match.Alliances.Blue)
+	// 	fmt.Printf("match.Alliances: RED  %+v\n", match.Alliances.Red)
+	// 	fmt.Printf("match.scorebreakdown: BLUE %+v\n", match.ScoreBreakdown.Blue)
+	// 	fmt.Printf("match.scorebreakdown: RED  %+v\n", match.ScoreBreakdown.Red)
+	// 	sortedKeys := make([]string, 0, len(match.ScoreBreakdown.Blue))
+	// 	fmt.Printf("\nmatch score key list:\n")
+	// 	for key := range match.ScoreBreakdown.Blue {
+	// 		sortedKeys = append(sortedKeys, key)
+	// 	}
+	// 	sort.Strings(sortedKeys)
+	// 	fmt.Printf("sorted key=%+v\n", sortedKeys)
+	// 	var header string
+	// 	for key := range sortedKeys {
+	// 		if len(header) == 0 {
+	// 			header = fmt.Sprintf("%+v", sortedKeys[key])
+	// 		} else {
+	// 			header = fmt.Sprintf("%s, %+v", header, sortedKeys[key])
+	// 		}
+	// 	}
+	// 	fmt.Printf("HEADER: %s\n", header)
+	// 	fmt.Println()
+	// 	fmt.Printf("match score value list:\n")
+	// 	var dataRow string
+	// 	for _, key := range sortedKeys {
+	// 		if len(dataRow) == 0 {
+	// 			dataRow = fmt.Sprintf("%+v", match.ScoreBreakdown.Blue[key])
+	// 		} else {
+	// 			dataRow = fmt.Sprintf("%s, %+v", dataRow, match.ScoreBreakdown.Blue[key])
+	// 		}
+	// 	}
+	// 	fmt.Printf("DATAROW: %s\n", dataRow)
+	// 	fmt.Println()
+
+	// var blueMap map[string]interface{}
+	// var redMap map[string]interface{}
+	// dataBlue, _ := json.Marshal(match.ScoreBreakdown.Blue)
+	// json.Unmarshal(dataBlue, &blueMap)
+	// dataRed, _ := json.Marshal(match.ScoreBreakdown.Red)
+	// json.Unmarshal(dataRed, &redMap)
+	// fmt.Printf("match.scorebreakdown: BLUE %+v\n", blueMap)
+	// fmt.Printf("match.scorebreakdown: RED  %+v\n", redMap)
+	// sortedKeys := make([]string, 0, len(blueMap))
+	// for key := range blueMap {
+	// 	sortedKeys = append(sortedKeys, key)
+	// 	fmt.Printf("%s ", key)
+	// }
+	// sort.Strings(sortedKeys)
+	// fmt.Println()
+	// for _, key := range sortedKeys {
+	// 	fmt.Printf(" %+v", blueMap[key])
+	// }
+	// fmt.Println()
+	// }
 
 	// report.TeamReport(t)
 
