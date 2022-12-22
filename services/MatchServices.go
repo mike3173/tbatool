244d59c4c0cfbebd402a5a653f083c0c30d3222f
Mike Buell <michael.buell@gmail.com>
2022-12-22 14:21:40 -0500

    Added other match bases enpoints

--- services/MatchServices.go
+++ services/MatchServices.go
@@ -2,10 +2,24 @@ package services
 
 import "fmt"
 
-const GETMATCHURL = "https://www.thebluealliance.com/api/v3/match/%s"
+const GETMATCHURL = "https://www.thebluealliance.com/api/v3/match/%s_%s%d"
+const GETEVENTMATCHESURL = "https://www.thebluealliance.com/api/v3/event/%s/matches"
+const GETEVENTMATCHESSIMPLEURL = "https://www.thebluealliance.com/api/v3/event/%s/matches/simple"
 
 func GetMatch(eventKey string, matchType string, matchNumber int) []byte {
-	var url string = fmt.Sprintf(GETMATCHURL, fmt.Sprintf("%s_%s%d", eventKey, matchType, matchNumber))
+	var url string = fmt.Sprintf(GETMATCHURL, eventKey, matchType, matchNumber)
+	var bodyBytes []byte = TBAMakeRequest(url)
+	return bodyBytes
+}
+
+func GetEventMatches(eventKey string) []byte {
+	var url string = fmt.Sprintf(GETEVENTMATCHESURL, eventKey)
+	var bodyBytes []byte = TBAMakeRequest(url)
+	return bodyBytes
+}
+
+func GetEventMatchesSimple(eventKey string) []byte {
+	var url string = fmt.Sprintf(GETEVENTMATCHESSIMPLEURL, eventKey)
 	var bodyBytes []byte = TBAMakeRequest(url)
 	return bodyBytes
 }
